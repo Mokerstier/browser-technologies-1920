45b85902b9bec672267616200526cfc6c1272e0d
Mokerstier <pannekoek-wouter@hotmail.com>
2020-03-20 19:42:38 +0100

    :pencil: rename file cuz heroku is CASE SENSITIVE

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,7 +1,7 @@
 function routes(){
 
     const render = require('.././controllers/render/render')
-    const getter = require('../controllers/getter/getbody')
+    const getter = require('.././controllers/getter/getbody')
 
     const express = require('express')
     const router = express.Router()
