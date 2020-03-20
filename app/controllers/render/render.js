35f53d2703818431bea79b44073ae31065b3bddd
Mokerstier <pannekoek-wouter@hotmail.com>
2020-03-20 19:08:42 +0100

    :mute: removing logs in render

--- app/controllers/render/render.js
+++ app/controllers/render/render.js
@@ -9,20 +9,11 @@ function renderFormOpen(req, res) {
     res.render('pages/intro.ejs', {
         title: `Hallo gebruiker`
     })
-    // console.log(res.locals.user)
-    // const user = res.locals.user
-    // if (!user){
-    //     res.render('pages/formintro.ejs', {
-    //     title: `Hallo gebruiker`        
-    //     })
-    // } else res.render('pages/formintro.ejs', {
-    //     title: `Hallo ${user.fname}`      
-    // })
 }
 
 function renderFormStart(req, res) {
     const user = res.locals.user
-    console.log(`hello ${user.fname}`)
+
     res.render('pages/q1.ejs', {
         title: 'Hallo gebruiker',
         user: user
@@ -31,7 +22,6 @@ function renderFormStart(req, res) {
 }
 function renderFormMid(req, res) {
     const user = res.locals.user
-    
 
     res.render('pages/q2.ejs', {
         title: 'Hallo gebruiker',
@@ -40,7 +30,6 @@ function renderFormMid(req, res) {
 }
 function renderFormEnd(req, res) {
     const user = res.locals.user
-    console.log(res.locals)
 
     res.render('pages/q3.ejs', {
         title: 'Hallo gebruiker',
@@ -49,7 +38,6 @@ function renderFormEnd(req, res) {
 }
 function renderFormCheck(req, res) {
     const user = res.locals.user
-    console.log(res.locals)
 
     res.render('pages/q4.ejs', {
         title: 'Hallo gebruiker',
