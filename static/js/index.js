c0607a4b886cf81a97aaf35d1609d0af990472a7
Mokerstier <pannekoek-wouter@hotmail.com>
2020-04-01 20:35:54 +0200

    :sparkles: fixed the form checker logic

--- static/js/index.js
+++ static/js/index.js
@@ -2,17 +2,28 @@ const jsWarning = document.getElementById("js-warning");
 const main = document.querySelector("main");
 const submit = document.querySelector("button[type=submit]");
 let inputCheck = document.querySelectorAll("fieldset input");
-const form = document.getElementById('enquete');
+const form = document.getElementById("enquete");
 const fieldsetContainer = document.querySelector(".fieldset-container");
 const fieldset = document.querySelectorAll("fieldset");
 const fieldsetIntro = document.getElementById("fieldset-intro");
-const text = document.querySelector("textarea");
+let text = document.querySelector("textarea");
 const progress = document.querySelector("progress");
 const koop = document.getElementById("koop");
 const backButton = document.querySelector(".button-back");
 const baseUrl = window.location.origin;
 
 let uuid = document.getElementById("uuid");
+
+function showKoop() {
+  let pos = 0;
+  console.log("pos-" + String(pos));
+  koop.classList.remove("pos-" + String(pos));
+  pos = Math.floor(Math.random() * 5);
+  koop.classList.add("pos-" + String(pos));
+}
+if (koop) {
+  showKoop();
+}
 
 if (uuid) {
   uuid = uuid.value;
@@ -32,6 +43,7 @@ if (typeof fetch !== "undefined") {
   getNextQuestion();
   checkForm();
 }
+// Client Side Fetch
 function getNextQuestion() {
   console.log(window.location.href);
   if (
@@ -93,52 +105,72 @@ function getNextQuestion() {
         // fetch-fieldset
         fieldsetQ3 = doc.getElementById("q3");
         addClass(fieldsetQ3, "fetched");
-
+        text = fieldsetQ3.querySelector('textarea')
         fieldsetContainer.appendChild(fieldsetQ3);
       })
       .catch(function(err) {
         // There was an error
         console.warn("Something went wrong.", err);
       });
-  } else {
-      for(i =0 ; i< fieldset.length; i++){
-          fieldset[i].removeAttribute('id')
-      }
-  }
+    }
+  // } else {
+  //   for (i = 0; i < fieldset.length; i++) {
+  //     fieldset[i].removeAttribute("id");
+  //   }
+  // }
 }
 
+function addClass(element, string) {
+  element.classList.add(string);
+}
+function removeClass(element, string) {
+  element.classList.remove(string);
+}
+function disableButton(button) {
+  button.setAttribute("disabled", "disabled");
+}
+function enableButton(button) {
+  button.removeAttribute("disabled");
+}
+
+let checked = 0;
 function checkCheckbox(fieldset) {
   if (fieldset) {
-    let checked = 0;
-
     let inputs = fieldset.querySelectorAll("input[type=checkbox]");
     let inputText = fieldset.querySelector("textarea");
     if (inputText) {
       if (inputText.value.length > 5) {
         enableButton(submit);
-        
+        progress.value = 9
+      } else {
+        removeClass(submit, "active");
+        disableButton(submit);
       }
     }
     for (i = 0; i < inputs.length; i++) {
       if (inputs[i].checked) {
         addClass(submit, "active");
-        inputs[i].setAttribute('value', 'checked')
+        inputs[i].setAttribute("value", "checked");
         enableButton(submit);
         console.log(inputs[i]);
         checked++;
       }
     }
     if (checked > 0) {
-        if(progress){
-            progress.value = startValue + 2;
+      if (progress) {
+        if (fieldset == fieldsetQ1) {
+          progress.value = startValue + 2;
+        } else if (fieldset == fieldsetQ2) {
+          progress.value = progress.value + 2;
         }
-      
+      }
+
       return true;
     } else {
-        if(progress){
-            progress.value = startValue;
-        }
-      
+      if (progress) {
+        progress.value = startValue;
+      }
+
       removeClass(submit, "active");
       disableButton(submit);
       return false;
@@ -148,37 +180,28 @@ function checkCheckbox(fieldset) {
 
     for (i = 0; i < inputCheck.length; i++) {
       if (inputCheck[i].checked) {
-        inputCheck[i].setAttribute('value', 'checked')
+        inputCheck[i].setAttribute("value", "checked");
         checked++;
       }
     }
     if (checked > 0) {
-        if(progress){
-            progress.value = progress.value + 2;
-        }
+      if (progress) {
+        progress.value = progress.value + 2;
+      }
       return true;
     } else {
-        if(progress){
-            progress.value = progress.value -2
-        }
+      if (progress) {
+        progress.value = progress.value - 2;
+      }
       return false;
     }
   }
-}
-function showKoop() {
-  let pos = 0;
-  console.log("pos-" + String(pos));
-  koop.classList.remove("pos-" + String(pos));
-  pos = Math.floor(Math.random() * 5);
-  koop.classList.add("pos-" + String(pos));
-}
-if (koop) {
-  showKoop();
 }
 
 function checkText() {
   if (!text) return false;
   if (text.value.length > 5) {
+    progress.value = 9;
     return true;
   } else return false;
 }
@@ -196,16 +219,19 @@ submit.addEventListener("click", function(event) {
   } else {
     if (count == 2) {
       form.setAttribute("action", "/q4");
-      form.submit()
+      form.submit();
     }
     event.preventDefault();
     // if(checkText()){
     //     button.classList.add('active')
     // }
-    let formData = Object.values(form).reduce((obj,field) => { obj[field.name] = field.value; return obj }, {})
-    
-    console.log(formData)
-    localStorage.setItem('data', JSON.stringify(formData) )
+    let formData = Object.values(form).reduce((obj, field) => {
+      obj[field.name] = field.value;
+      return obj;
+    }, {});
+
+    console.log(formData);
+    localStorage.setItem("data", JSON.stringify(formData));
 
     switch (count) {
       case 0:
@@ -225,16 +251,16 @@ submit.addEventListener("click", function(event) {
         fieldsetQ2.classList.toggle("unshow");
         fieldsetQ3.classList.toggle("show");
         count = count + 1;
-        console.log(count)
+        console.log(count);
         break;
     }
   }
 });
 
 backButton.addEventListener("click", function(event) {
-    if(window.location.href === String(baseUrl)+'/q4'){
-        return true
-    }
+  if (window.location.href === String(baseUrl) + "/q4") {
+    return true;
+  }
   event.preventDefault();
 
   count = count % 3;
@@ -242,6 +268,8 @@ backButton.addEventListener("click", function(event) {
   switch (count) {
     case 1:
       removeClass(fieldsetQ2, "show");
+      addClass(submit, "active");
+      enableButton(submit);
       checkField = fieldsetQ1;
       fieldsetQ1.classList.toggle("unshow");
       fieldsetQ1.classList.toggle("show");
@@ -256,18 +284,7 @@ backButton.addEventListener("click", function(event) {
       break;
   }
 });
-function addClass(element, string) {
-  element.classList.add(string);
-}
-function removeClass(element, string) {
-  element.classList.remove(string);
-}
-function disableButton(button) {
-  button.setAttribute("disabled", "disabled");
-}
-function enableButton(button) {
-  button.removeAttribute("disabled");
-}
+
 function checkIntro() {
   if (fieldsetIntro) {
     if (fieldsetIntro.querySelector("input").value) {
@@ -276,43 +293,54 @@ function checkIntro() {
   }
 }
 function checkForm() {
-  console.log("checkform adsad");
-  console.log(checkText())
-  if (checkText() || checkCheckbox() || checkIntro()) {
+  console.log(checkIntro(), checkText(), checkCheckbox());
+  if (text) {
+    if (text.value.length < 5) {
+      console.log("niet genoeg");
+
+      removeClass(submit, "active");
+      disableButton(submit);
+    }
+  }
+  if (checkText() || checkCheckbox(checkField) || checkIntro()) {
     addClass(submit, "active");
     enableButton(submit);
   } else {
+    console.log("inactief");
+
     removeClass(submit, "active");
     disableButton(submit);
   }
+   
 }
 
 window.addEventListener("load", function(event) {
   if (form) {
-      console.log(fieldsetQ1)
-    if (fieldsetQ1) {
-      console.log("will check form1");
-      checkCheckbox(fieldsetQ1);
-    } else {
-      checkForm();
-    }
-  }
-});
-form.addEventListener("keyup", function(event) {
-  if (form) {
-
-    if (checkField) {
+    if (!checkField == fieldsetQ3) {
       checkCheckbox(checkField);
     } else {
       checkForm();
     }
   }
 });
+
+form.addEventListener("keyup", function(event) {
+  console.log('check al die dingen');
+  
+  if (form) {
+    if (!checkField == fieldsetQ3) {
+      console.log('hallo');
+      
+      checkCheckbox(checkField);
+    } else {
+      checkForm();
+    }
+  }
+});
+
 form.addEventListener("change", function(event) {
   if (form) {
-    console.log(checkField);
-    if (checkField) {
-     
+    if (!checkField == fieldsetQ3) {
       checkCheckbox(checkField);
     } else {
       checkForm();
